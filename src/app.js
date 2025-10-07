f4c891cdb3e18d05b1b9c53ed27726bc1c7a8d93
Camiloler14 <kamiilermald@gmail.com>
2025-10-07 14:54:26 -0500

    Configuracion de test

--- src/app.js
+++ src/app.js
@@ -14,14 +14,23 @@ dotenv.config({ quiet: true });
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
+
 
 app.use("/api/usuario", usuarioRoutes);
 app.use("/api/estudiante", estudianteRoutes);
 app.use("/api/docente", docenteRoutes);
 app.use("/api/materia", materiaRoutes);
 app.use("/api/inscripciones", estudianteMateriaRoutes);
+
 
 swaggerDocs(app);
 
